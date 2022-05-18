91986108effb5f6aeecb1434b4baf104641dc912
Amitava Chakraborty <camitavasubs@gmail.com>
2022-05-18 15:24:11 +0530

    Update 021-Antipatters.py
    Changed

--- 021-Antipatters.py
+++ 021-Antipatters.py
@@ -10,7 +10,17 @@ class Rectangle(object):
 #The following code assigns a lambda function which returns the double of its input to a variable. This is functionally identical to creating a def.
 
         f = lambda x: 2 * x
-        
+
+#Assigning to built-in function
+#Python has a number of built-in functions that are always accessible in the interpreter. Unless you have a special reason, you should neither overwrite these functions nor assign a value to a variable that has the same name as a built-in function. Overwriting a built-in might have undesired side effects or can cause runtime errors. Python developers usually use built-ins ‘as-is’. If their behaviour is changed, it can be very tricky to trace back the actual error.
+
+#In the code below, the list built-in is overwritten. This makes it impossible, to use list to define a variable as a list. As this is a very concise example, it is easy to spot what the problem is. However, if there are hundreds of lines between the assignment to list and the assignment to cars, it might become difficult to identify the problem.
+# Overwriting built-in 'list' by assigning values to a variable called 'list'
+        list = [1, 2, 3]
+# Defining a list 'cars', will now raise an error
+        cars = list()
+# Error: TypeError: 'list' object is not callable        
+
 r = Rectangle(5, 6)
 # direct access of protected member
 print("Width: {:d}".format(r._width))
