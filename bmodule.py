f58e5f471d1f8e757cec939273eb7eff9c29d34a
Amitava Chakraborty <camitavasubs@gmail.com>
2022-05-18 16:32:52 +0530

    Update bmodule.py

--- bmodule.py
+++ bmodule.py
@@ -6,18 +6,9 @@ from amodule import aMODULE
 
 
 def f():
-<<<<<<< HEAD
-    return cmodule.e
+    return cmodule.g
 	
 print (f())
-aMODULE.actionX()
-=======
-    return cmodule.g
-
-
-print(f())
-
->>>>>>> 8bd440f1587a1ccf2099c3b716232ec3e7ab86f2
 
 def cleanup(self):
     return
